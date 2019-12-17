9227533f31d22f2facb632cd1e87928b7344e2ec
Junwei Zhang <zjunweihit@163.com>
2019-12-17 13:25:32 +0800

    Convenet1D for IMDB data

---
+++ conv1D.py
@@ -0,0 +1,37 @@
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+from pynlp.plot.historyplot import HistoryPlot
+
+max_features = 10000
+max_len = 500
+N_epochs = 10
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+
+model = Sequential()
+model.add(layers.Embedding(max_features, 128, input_length=max_len))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.MaxPool1D(5))
+model.add(layers.Conv1D(32, 7, activation='relu'))
+model.add(layers.GlobalMaxPooling1D())
+model.add(layers.Dense(1))
+model.summary()
+
+model.compile(optimizer=RMSprop(lr=1e-4),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+H = model.fit(x_train, y_train,
+              epochs=N_epochs,
+              batch_size=128,
+              validation_split=0.2)
+
+hp = HistoryPlot(N_epochs)
+hp.show(H)
